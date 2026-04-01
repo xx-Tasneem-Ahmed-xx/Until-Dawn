b8d6be7640184ce1ad9263bfe61f012607dc54fb
Tasneem Ahmed <tasneem.mostafa04@eng-st.cu.edu.eg>
2026-04-01 18:26:28 +0200

    feat: transform class and vertex shader

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -2,6 +2,7 @@
 #include "../deserialize-utils.hpp"
 
 #include <glm/gtx/euler_angles.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace our {
 
@@ -9,8 +10,11 @@ namespace our {
     // Remember that the order of transformations is: Scaling, Rotation then Translation
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
     glm::mat4 Transform::toMat4() const {
-        //TODO: (Req 3) Write this function
-        return glm::mat4(1.0f); 
+        glm::mat4 scale_matrix = glm::scale(glm::mat4(1.0f), scale);
+        glm::mat4 rotation_matrix = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
+        glm::mat4 translation_matrix = glm::translate(glm::mat4(1.0f), position);
+
+        return translation_matrix * rotation_matrix * scale_matrix;
     }
 
      // Deserializes the entity data and components from a json object
