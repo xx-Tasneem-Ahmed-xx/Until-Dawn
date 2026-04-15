98897ff7141b10ed2369763a3ae05d764a19bb52
Tasneem Ahmed <tasneem.mostafa04@eng-st.cu.edu.eg>
2026-04-15 13:46:15 +0200

    feat: update mesh utils, texture utils and asset loader to support glb

--- source/common/mesh/mesh-utils.hpp
+++ source/common/mesh/mesh-utils.hpp
@@ -3,10 +3,13 @@
 #include "mesh.hpp"
 #include <string>
 
-namespace our::mesh_utils {
+namespace our::mesh_utils
+{
     // Load an ".obj" file into the mesh
-    Mesh* loadOBJ(const std::string& filename);
+    Mesh *loadOBJ(const std::string &filename);
+    // Load a ".glb" file into the mesh
+    Mesh *loadGLB(const std::string &filename);
     // Create a sphere (the vertex order in the triangles are CCW from the outside)
     // Segments define the number of divisions on the both the latitude and the longitude
-    Mesh* sphere(const glm::ivec2& segments);
+    Mesh *sphere(const glm::ivec2 &segments);
 }
